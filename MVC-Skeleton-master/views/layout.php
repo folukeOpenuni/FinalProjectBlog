4811a6d2cc365970ed5c3e8e87caab7f094b02d5
Alice Gill <alicemaygill@gmail.com>
2018-04-11 20:46:27 +0100

    no changes

--- MVC-Skeleton-master/views/layout.php
+++ MVC-Skeleton-master/views/layout.php
@@ -1,16 +1,62 @@
 <!DOCTYPE html>
 <html>
-  <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+<!-- REQUIRED META TAGS -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>Gap Yah Blog</title>
+<!-- BOOTSTRAP CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<!-- STYLES CSS-->
+    <link href="css/styles.css" rel="stylesheet" type="text/css"/>
+<!--W3SCHOOLS CSS-->
+    <link rel="stylesheet" href="https://www.w3schools.com/lib/w3.css">
+<!--GOOGLE FONT-->
+    <link href="https://fonts.googleapis.com/css?family=Delius+Swash+Caps" rel="stylesheet">
+    <title>Gap Yah Blog</title>
+    </head>
+    
+    <body>
+<!-- NAV BAR -->
+<nav class="navbar navbar-expand-lg navbar-light bg-white">
+    <a class="navbar-brand" href="#"><img style="height: 30px; width: 30px;" src="../images/world-icon.jpg">  Gap Yah ltd.</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+            <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Blog</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Continents
+            </a>
+            <div class="dropdown-menu" aria-labelledby="continentDropdown">
+                <a class="dropdown-item" href="#">Africa</a>
+                <a class="dropdown-item" href="#">Antarctica</a>
+                <a class="dropdown-item" href="#">Asia</a>
+                <a class="dropdown-item" href="#">Australia</a>
+                <a class="dropdown-item" href="#">Europe</a>
+                <a class="dropdown-item" href="#">North America</a>
+                <a class="dropdown-item" href="#">South America</a>
+            </div>
+        </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search blog posts" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>      
 
-<link rel="stylesheet" href="https://www.w3schools.com/lib/w3.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Tangerine">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Pacifico|Pangolin" >
-<link rel="stylesheet" href="views/css/styles.css">
-<title>Shopping Cart</title>
-  </head>
-  <body>
-    <header class="w3-container w3-gray">
+<!--    <header class="w3-container w3-gray">
       <a href='/MVC_Skeleton'>Home</a>
       <a href='?controller=product&action=readAll'>Products</a>
       <a href='?controller=product&action=create'>Add Product</a>
@@ -18,10 +64,46 @@
 <div class="w3-container w3-pink">
     <?php require_once('routes.php'); ?>
 </<div>
-<div class="w3-container w3-gray">
-    <footer >
+</div>-->
+
+<!--FOOTER-->
+<footer class="page-footer bg-f">
+    <div class="container-fluid text-center">
+    <div class="row">
+
+<!--FIRST FOOTER COL-->
+        <div class="col-md-4">
+            <h5>Get in touch</h5>
+            <p>Call: 0113 2234455</p>
+            <p>Email: <a href="mailto:hello@gapyah.com" style="color:white">hello@gapyah.com</a></p>
+        </div>
+<!--SECOND FOOTER COL-->
+        <div class="col-md-4">
+            <h5>Join our online community</h5>
+            <br>
+            <button type="button" class="btn btn-lg btn-light" href="#">Sign up</button>
+        </div>
+<!--THIRD FOOTER COL-->
+        <div class="col-md-4 text-center">
+            <h5>Featured posts</h5>
+                <ul class="list-unstyled">
+                    <li>
+                        <a href="#" style="color:white">How to prepare for 6 months traveling</a>
+                    </li>
+                    <li>
+                        <a href="#" style="color:white">8 reasons to visit South East Asia in 2018</a>
+                    </li>
+                    <li>
+                        <a href="#" style="color:white">How to budget Down Under</a>
+                    </li>
+                </ul>
+        </div>
+        </div>
+    </div>
+</footer>
         Copyright &COPY; <?= date('Y'); ?>
-    </footer>
-</div>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   </body>
 </html>
--- MVC-Skeleton-master/views/pages/home.php
+++ MVC-Skeleton-master/views/pages/home.php
@@ -1,4 +1,59 @@
-<p>Hello there <?php echo $first_name . ' ' . $last_name; ?>!<p>
-<p>The above data is present to demonstrate the utilisation of variables 
-populated earlier within the page processing</p>
-<p>This is the home page of the MVC Skeleton Application</p>
+<?php 
+//require_once '/Applications/XAMPP/xamppfiles/htdocs/FinalProjectBlog/MVC-Skeleton-master/views/layout.php'; ?>
+
+<!--HEADER CAROUSEL-->
+<div id="headercarousel" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active text-overlay">
+        <div class="text-overlay">
+        <img class="d-block w-100" src="../images/views.jpg" alt="guy looking over a bay">
+        <div class="text-centered">On my Gap Yah...</div>
+        </div>
+    </div>
+    <div class="carousel-item">
+        <div class="text-overlay"> 
+        <img class="d-block w-100" src="../images/waterfall.jpg" alt="couple at waterfall">
+        <div class="text-centered">On my Gap Yah...</div>
+        </div>
+   </div>
+   <div class="carousel-item">
+      <div class="text-overlay">  
+      <img class="d-block w-100" src="../images/travel.jpg" alt="friends travelling">
+      <div class="text-centered">On my Gap Yah...</div>
+      </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#headercarousel" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#headercarousel" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>        
+
+<!--3 IMAGE SECTION-->
+        <section class="container-fluid">
+            <h2 class="text-center">April's top 3 destinations</h2>
+            <div class="row">   
+                <div class="col-md-4 container">
+                    <img class="image" src="../images/norway.jpg" class="img-responsive margin" width="100%" alt="Norwegian landscape"/>
+                    <div class="middle">
+                    <div class="text">10 Instagrammable places in Norway</div>   
+                    </div>
+                </div>
+                <div class="col-md-4 container">
+                    <img class="image" src="../images/sanfrancisco.jpg" class="img-responsive rounded" width="100%" alt="Street in Sanfrancisco"/>
+                    <div class="middle">
+                    <div class="text">Top 10 cheap eats in Sanfran</div>
+                    </div>
+                </div>
+                <div class="col-md-4 container">
+                    <img class="image" src="../images/greece.jpg" class="img-responsive rounded" width="100%" alt="Greek beach">
+                    <div class="middle">
+                    <div class="text">My secret guide to the best beaches in Greece</div>
+                    </div>
+            </div>
+            </div>                
+        </section>
