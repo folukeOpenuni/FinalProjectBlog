14e2ab3adaf263decbfe1b39808a9555ea5a8166
Naomi <naomi.cooper677@gmail.com>
2018-04-15 18:16:29 +0100

    About Us page formated

--- MVC-Skeleton-master/views/layout.php
+++ MVC-Skeleton-master/views/layout.php
@@ -23,13 +23,13 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
         <li class="nav-item active">
-            <a class="nav-link" href='/MVC-Skeleton'>Home<span class="sr-only">(current)</span></a>
+            <a class="nav-link" href='?controller=pages&action=home'>Home<span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item">
             <a class="nav-link" href='?controller=blogpost&action=readAll'>Blog</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
+          <a class="nav-link" href='?controller=pages&action=about'>About</a>
         </li>
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
