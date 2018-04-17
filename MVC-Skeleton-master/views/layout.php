5910f799001b868e04eca991f2a2345cf203ca14
IoneMurray1 <ionemurray1@gmail.com>
2018-04-17 17:52:55 +0100

    Changed POST to GET in layout
    Only 1 error message now rather than 3

--- MVC-Skeleton-master/views/layout.php
+++ MVC-Skeleton-master/views/layout.php
@@ -53,10 +53,11 @@
     <form class="form-inline my-2 my-lg-0" action="?controller=blogpost&action=find" method="post" name="name" >
       <input class="form-control mr-sm-2" type="search" placeholder="Search blog posts"aria-label="Search" name="name">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit" value = "search">Search</button>
-      <?php if(isset($_POST['name'])){
-       
-        if(preg_match("/[A-Z | a-z]+/", $_POST['name'])){
-      $name=$_POST['name'];}}?>
+     
+ <?php if(isset($_GET['name'])){
+       //Ione changed to GET from POST
+        if(preg_match("/[A-Z | a-z]+/", $_GET['name'])){
+      $name=$_GET['name'];}}?>
     </form>
   </div>
 </nav>      
