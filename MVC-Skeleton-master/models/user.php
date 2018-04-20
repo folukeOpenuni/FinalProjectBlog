9ff241094217fc02cc7e54036721d8d4f7c5c815
IoneMurray1 <ionemurray1@gmail.com>
2018-04-20 21:04:06 +0100

    ghjk
    fgcvhbjn

--- MVC-Skeleton-master/models/user.php
+++ MVC-Skeleton-master/models/user.php
@@ -27,8 +27,55 @@
     $this->Password = $Password;
     $this->CountryID = $CountryID;
     }
-
-    
+  
+     
+      // username and password sent from form 
+//        if($_SERVER["REQUEST_METHOD"] == "POST") {
+//      $Email = mysqli_real_escape_string($db,$_POST['Email']);
+//      $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
+//      
+//      $sql = "SELECT id FROM admin WHERE username = '$myusername' and passcode = '$mypassword'";
+//      $result = mysqli_query($db,$sql);
+//      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+//      $active = $row['active'];
+//      
+//      $count = mysqli_num_rows($result);
+      
+      // If result matched $myusername and $mypassword, table row must be 1 row
+//		
+//      if($count == 1) {
+//         session_register("myusername");
+//         $_SESSION['login_user'] = $myusername;
+//         
+//         header("location: welcome.php");
+//      }else {
+//         $error = "Your Login Name or Password is invalid";
+//      }
+//   }
+//   
+//find function from blogpost
+//        public static function find() {
+//      $db = Db::getInstance();
+//      //use intval to make sure $id is an integer
+//      if(isset($_POST['name'])){
+//       
+//        if(preg_match("/[A-Z | a-z]+/", $_POST['name'])){
+//            $name=$_POST['name'];
+//        }
+//      }
+//      $req = $db->query("Select blogpost.BlogPostID, blogpost.Title, blogpost.Image, blogpost.DatePublished, blogpost.WriterID, blogpost.Content, country.Country, continent.Continent
+//From blogpost
+//Inner join blogpostcountry on blogpostcountry.BlogPostID = blogpost.BlogPostID
+//Inner Join country ON blogpostcountry.CountryID = country.CountryID
+//Inner Join continent on country.ContinentID = continent.ContinentID where Title like '%".$name."%' OR Country like '%".$name."%'OR Continent like '%".$name."%'");
+//     foreach($req->fetchAll() as $blogpost) {
+// $list[] = new blogpost($blogpost['BlogPostID'],$blogpost['Title'], $blogpost['Image'], $blogpost['DatePublished'], $blogpost['WriterID'], $blogpost['Content'], $blogpost['Country'], $blogpost['Continent']);
+//
+//}
+//   return $list;
+// }
+    //end of find function from blogpost class
+ 
     
         public static function add() {
     $db = Db::getInstance();
