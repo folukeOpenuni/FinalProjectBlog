876e98bdbb2c0e6baa65614f050cb5e732d5c730
Naomi <naomi.cooper677@gmail.com>
2018-04-21 19:18:44 +0100

    Read updated

--- MVC-Skeleton-master/views/blogposts/read.php
+++ MVC-Skeleton-master/views/blogposts/read.php
@@ -9,7 +9,7 @@
 </style>
 
 <div class="text-white text-overlay blog-img">
-  <img class="img-fulid blog-img" src="views/images/<?php echo $blogpost->Image; ?>">
+  <img class="img-fulid blog-img" src="views/images/<?php echo $blogpost->HeaderImage; ?>">
     <h1 class="text-centered"><?php echo $blogpost->Title; ?></h1>
     <p class="text-subhead">Date Published: <?php echo $blogpost->DatePublished; ?></p>
 </div>
@@ -17,39 +17,23 @@
 <br>
 <center>
 
-    <img src="views/images/profilepicture/<?php echo $blogpost->WriterID;?>.jpg" alt="Avatar" class="avatar">
+    <img src="views/images/profilepicture/<?php echo $blogpost->WriterFName;?>.jpeg" alt="Avatar" class="avatar">
     <br>
     <br>
-    <p>Written by <?php 
-    if ($blogpost->WriterID == 1){
-        echo "John Murray";
-    } elseif ($blogpost->WriterID == 2) {
-        echo "Andrea Orset";
-    }elseif ($blogpost->WriterID == 3){
-        echo "Penny Yiota";
-    } else{
-        echo "Keira Kenny";
-    }
+    <p>Written by <?php echo $blogpost->WriterFName;
     
 ?></p>
 
 
 <!--<p>Product BlogPostID: <?php //echo $blogpost->BlogPostID; ?></p>-->
 <div class="jumbotron">
-<p><?php echo $blogpost->Content; ?></p>
+<p><?php echo $blogpost->ContentP1; ?></p>
 </div>
+    
+    <!--PLACEHOLDER FOR CAROSELL-->
+   <div class="jumbotron">
+<p><?php echo $blogpost->ContentP2; ?></p>
+</div> 
 
-<!--DO WE NEED THE BELOW? OR CAN WE MAKE A PHOTO MANDATORY WHEN UPLOADING A BLOG-->
-<?php 
-$file = 'views/images/' . $blogpost->Image;
-if(file_exists($file)){
-    $img = "<img src='$file' width='150' />";
-    echo $img;
-}
-else
-{
-echo "<img src='views/images/standard/_noproductimage.png' width='150' />";
-}
 
-?>
 </center>	
