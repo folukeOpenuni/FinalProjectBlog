04371be1b8d7543f557e957c83e1b3c4357c12b2
Naomi <naomi.cooper677@gmail.com>
2018-04-22 12:13:24 +0100

    Uploader working - minus 3 secondary images

--- MVC-Skeleton-master/views/blogposts/create.php
+++ MVC-Skeleton-master/views/blogposts/create.php
@@ -1,8 +1,7 @@
+<p>Fill in the following form to create a post:</p>
 <form action="" method="POST" class="w3-container" enctype="multipart/form-data">
-    <div class="left-pad">
-    <h1>Create Post</h1>
-    <p>Fill in the form below to create a post:</p>
-    </div>
+    <h2>Create Post</h2>
+    
     <p>
         <div class="form-group col-md-12">
             <label>Title</label><br>
@@ -51,9 +50,9 @@
        <option value="7">Relaxation</option>
   </select>
         </div>
-<div class="form-group col-md-12">
-<label for="CountryID">Country</label>
-<select class="form-control" id="CountryID" name='CountryID'>
+    <div class="form-group col-md-12">
+  <label for="CountryID">Country</label>
+    <select class="form-control" id="CountryID" name='CountryID'>
     <option value="1">Afghanistan</option>
     <option value="2">Albania</option>
     <option value="3">Algeria</option>
@@ -266,9 +265,9 @@
     <p>Upload 1 featured image:</p> <input type="file" name="myUploader"/>
     <br>
     <br>
-    <input type="hidden" name="MAX_FILE_SIZE" value="10000000"/>
+    <!--<input type="hidden" name="MAX_FILE_SIZE" value="10000000"/>
     <p>Upload up to 3 secondary images:</p> <input id="myUploader" type="file" name="myUploader" multiple/>
-    <br>
+    <br>-->
     <br>
     <input class="btn btn-secondary" type="submit" value="submit">
 </div>
