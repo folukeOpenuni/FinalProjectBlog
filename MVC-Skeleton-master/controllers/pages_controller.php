72e8261335ccde537b15bed8e6d6fe21e7cf9244
IoneMurray1 <ionemurray1@gmail.com>
2018-04-22 16:04:10 +0100

    added getBudget to pages_controller
    rfedws

--- MVC-Skeleton-master/controllers/pages_controller.php
+++ MVC-Skeleton-master/controllers/pages_controller.php
@@ -17,4 +17,39 @@ class PagesController {
       require_once('views/pages/error.php');
     }
     
+    public function getBudget(){
+        echo "test";
+//$q = intval($_GET['q']);
+    $db = Db::getInstance();
+      //use intval to make sure $id is an integer
+    //$BlogPostID = intval($BlogPostID);
+    $req = $db->prepare("SELECT * FROM country WHERE CountryID = '".$q."'");
+      //the query was prepared, now replace :id with the actual $id value
+      $req->execute();
+      $result = $req->fetch();
+      
+if($result){
+echo "<table>
+<tr>
+<th>Country</th>
+<th>PriceofPint</th>
+<th>PriceofHostel</th>
+</tr>";
+while($row = mysqli_fetch_array($result)) {
+    echo "<tr>";
+    echo "<td>" . $row['Country'] . "</td>";
+    echo "<td>" . $row['PriceofPint'] . "</td>";
+    echo "<td>" . $row['PriceofHostel'] . "</td>";
+    echo "</tr>";
+}
+echo "</table>";
+      
+      
+}
+    else
+    {
+        //replace with a more meaningful exception
+        throw new Exception('A real exception should go here');
+    }
+    }
 }
