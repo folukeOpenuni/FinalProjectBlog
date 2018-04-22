bd1c7c30a9ce2984e1eb2680a7748ca1d48aefe9
IoneMurray1 <ionemurray1@gmail.com>
2018-04-22 23:12:58 +0100

    Added Ajax connection
    Added Ajax connection

---
+++ MVC-Skeleton-master/ajax.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+    
+
+        <?php
+    require_once('connection.php');
+        
+    if (isset($_GET['controller']) && isset($_GET['action'])) {
+        $controller = $_GET['controller'];
+        $action     = $_GET['action'];
+  } else {
+        $controller = 'pages';
+        $action     = 'home';
+  }
+require_once('routes.php');
+    #require_once('views/layout.php');
+        ?>
+
