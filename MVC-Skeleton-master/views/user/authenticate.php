016f259b8736cd187c7e0cdd44613e110adbe1c2
Alice Gill <alicemaygill@gmail.com>
2018-04-22 11:02:23 +0100

    authenticate

--- MVC-Skeleton-master/views/user/authenticate.php
+++ MVC-Skeleton-master/views/user/authenticate.php
@@ -1,22 +1,22 @@
-<p>Join the Gap Yah Community!:</p>
-<form action="" method="POST" class="w3-container" enctype="multipart/form-data">
+<div class="text-white text-overlay">
+    <img class="img-fluid" src="views/images/friends.jpg" alt="crowd of friends at sunset">
+    <h1 class="text-centered">Login</h1>
 </div>
-    <div>
-        <p>
-        <input class="w3-input" type="email" name="Email" required>
-        <label>Email</label>
-    </p>
-         <p>
-    
-           <input class="w3-input" type="password" name="Password" required>
-        <label>Password</label>
-    </p>
-    
-     <p>
-    <input class="w3-btn w3-pink" type="submit" value="Submit">
-  </p>
-  
-    </div>
+<div class="left-pad top-pad">
+<p>Login to your Gap Yah account:</p>
+<form action="" method="POST" class="w3-container" enctype="multipart/form-data">
+        <div class="form group col-md-12">
+        <label>Email</label><br>
+        <input class="w3-input" type="email" name="Email" required placeholder="your@email.com">
+        </div>
+        <div class="form-group col-md-12">
+        <br>
+        <label>Password</label><br>
+            <input class="w3-input" type="password" name="Password" required>
+        </div>
+        <p class="left-pad">
+            <input class="btn btn-secondary" type="submit" value="Submit">
+        </p>
    
-    
 </form>
+</div>
